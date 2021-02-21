eefcbbe605a9528b88988ef0387340f11043977f
Wimalaweera K.T <thushankavindu2266@gmail.com>
2021-02-21 20:34:16 +0530

    Create navigation true component

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Blog from '../views/Blog.vue'
+import About from '../views/About.vue'
 
 
 Vue.use(VueRouter)
@@ -10,6 +12,16 @@ const routes = [
         path: '/',
         name: 'Home',
         component: Home
+    },
+    {
+        path: '/blog',
+        name: 'Blog',
+        component: Blog
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About
     }
 ]
 
