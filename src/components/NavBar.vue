b1c0793317007370f024452fca55801d1d0af6d5
Wimalaweera K.T <thushankavindu2266@gmail.com>
2021-02-21 16:06:37 +0530

    add all devDependencies

---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <v-toolbar flat class="align-center">
+
+      <v-btn to="/" text color="#00BD9D">
+        Home
+      </v-btn>
+
+      <v-btn to="/projects" text color="#00BD9D">
+        Projects
+      </v-btn>
+
+      <v-btn to="/about" text color="#00BD9D">
+        About
+      </v-btn>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+}
+</script>
+
+<style scoped>
+
+</style>
